897aeae1ee0914dcc4d6af2c573dcf5faf60574d
Hasuzawa <hasuzawa.yuichi.international@gmail.com>
2023-06-11 20:34:12 +0900

    add: literal string for some token

--- parser/scanner/token/token.go
+++ parser/scanner/token/token.go
@@ -33,20 +33,24 @@ const (
 
 	// string comparison operators
 
+	// contains
 	CONTAINS
+	// beginswith
 	BEGINSWITH
+	// endswith
 	ENDSWITH
+	// like
 	LIKE
 
 	// logical operators
 
-	// AND
+	// and
 	AND
-	// OR
+	// or
 	OR
-	// ALL
+	// all
 	ALL
-	// ANY
+	// any
 	ANY
 
 	// reserved words
@@ -84,3 +88,66 @@ const (
 	// ,
 	COMMA
 )
+
+// some token has one and only one valid (or preferred) representation. This facilitates formatting token.
+func (t Token) Literal() string {
+	switch t {
+	case EQL:
+		return "=="
+	case NEQ:
+		return "!="
+	case LT:
+		return "<"
+	case LE:
+		return "<="
+	case GT:
+		return ">"
+	case GE:
+		return ">="
+	case CONTAINS:
+		return "contains"
+	case BEGINSWITH:
+		return "beginswith"
+	case ENDSWITH:
+		return "endswith"
+	case LIKE:
+		return "like"
+	case AND:
+		return "and"
+	case OR:
+		return "or"
+	case ALL:
+		return "all"
+	case ANY:
+		return "any"
+	case YES:
+		return "YES"
+	case NO:
+		return "NO"
+	case TRUE:
+		return "true"
+	case FALSE:
+		return "false"
+	case NULL:
+		return "NULL"
+	case NIL:
+		return "nil"
+	case LPAREN:
+		return "("
+	case LBRACK:
+		return "["
+	case LBRACE:
+		return "{"
+	case RPAREN:
+		return ")"
+	case RBRACK:
+		return "]"
+	case RBRACE:
+		return "}"
+	case COMMA:
+		return ","
+	default:
+		break
+	}
+	return ""
+}
